74b41644bc655252eea854223ddd8fa08434462a
bomurf <bomurf@hotmail.com>
2020-04-03 11:03:23 +0900

    created stubs, variables

---
+++ app.js
@@ -0,0 +1,26 @@
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const btn_reset = document.getElementsByClassName("btn-reset");
+let missed = 0;
+
+// return a random phrase from an array
+const getRandomPhraseAsArray = arr => {};
+
+getRandomPhraseAsArray(phrases);
+
+// adds the letters of a string to the display
+const addPhraseToDisplay = arr => {};
+
+addPhraseToDisplay(phraseArray);
+
+// check if a letter is in the phrase
+const checkLetter = button => {};
+
+// check if the game has been won or lost
+const checkWin = () => {};
+
+// listen for the start game button to be pressed
+startButton.addEventListener("click", () => {});
+
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener("click", e => {});
